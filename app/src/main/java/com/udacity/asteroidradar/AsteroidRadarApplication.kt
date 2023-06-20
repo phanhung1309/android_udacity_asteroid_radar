edbcb070d3e65b846c51792e333df9d8bc27d24e
Hung Phan <phanhung1309@gmail.com>
2023-06-20 08:02:09 +0700

    create RefreshAsteroidsWorker

---
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -0,0 +1,47 @@
+package com.udacity.asteroidradar
+
+import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.udacity.asteroidradar.work.RefreshAsteroidsWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class AsteroidRadarApplication : Application() {
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork() {
+        val constants = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
+        val repeatingRequest =
+            PeriodicWorkRequestBuilder<RefreshAsteroidsWorker>(1, TimeUnit.DAYS)
+                .setConstraints(constants)
+                .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshAsteroidsWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.UPDATE,
+            repeatingRequest
+        )
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+}
